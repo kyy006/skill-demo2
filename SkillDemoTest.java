174fb10dffef0c22707de50e08b12a3f2d8f3ac2
kyy006 <kyy006@ucsd.edu>
2022-02-03 19:29:05 -0800

    pass

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void addition() {
-        assertEquals(2, SkillDemo.add(1,3));//expect fail
+        assertEquals(2, SkillDemo.add(1,1));//expect pass
     }
 }
